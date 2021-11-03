01596950bffbde76cc7248b12dd28b200200d971
Patricia Baldez <patriciabaldez1985@gmail.com>
2021-11-03 17:51:02 -0300

    continuacao do cod

--- src/pages/ProfilePage/styled.js
+++ src/pages/ProfilePage/styled.js
@@ -1,67 +1,90 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
-export const ContainerProfilePage = styled.div`
-width: 100vw;
-height: 100vh;
-display: flex;
-align-items: center;
-flex-direction: column;
-justify-content: center;
-margin-bottom: 80px; 
-position: relative;
-
+export const MainContainerProfilePage = styled.div`
+  position: relative;
+  height: 100vh;
 `
-export const RenderContainer = styled.div`
-width: 100vw;
-overflow: scroll;
-box-sizing: border-box;
-padding: 0px;
-min-height:60vh;
- margin-bottom: 80px; 
+export const EditAddress = styled.img`
+  right: 30px;
+  object-fit: contain;
+  position: absolute;
 `
-export const TitleLogout = styled.div`
-
+export const EditProfile = styled.img`
+  width: 24px;
+  height: 24px;
+  margin: 16px 16px 2px 320px;
+  object-fit: contain;
+  position: absolute;
 `
-export const LogoutContainer = styled.div`
-display: flex;
-align-items: center;
-justify-content: space-between;
-padding: 0px 10px;
-
+export const OrderBar = styled.h4`
+  width: 360px;
+  height: 20px;
+  margin-top: 10px;
+  margin: auto;
+  margin-bottom: 8px;
+  text-align: start;
+  padding-bottom: 5px;
+ border-bottom: 1px solid black;
+ font-weight: lighter;
+`
+export const UserData = styled.p`
+  margin: 8px 0 0 16px;
+  font-family: Roboto;
+  font-size: 16px;
+  font-weight: bolder;
+  letter-spacing: -0.39px;
+  color: black;
+`
+export const AddresTitle = styled.p`
+  margin: 8px 0px 0px 16px;
+  font-family: "Roboto";
+  font-size: 16px;
+  font-weight: bolder;
+  letter-spacing: -0.39px;
+  color: #b8b8b8;
 `
 export const UserContainer = styled.div`
-
+  width: 360px;
+  height: 106px;
+  margin: 0px 0;
+  padding: 5px;
+  position: relative;
 `
-export const UserData = styled.div`
-margin: 8px 0 0 16px;
-font-family: Roboto;
-font-size: 16px;
-
+export const AddressContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  width: 100%;
+  height: 76px;
+  padding: 16px 0px;
+  background-color: #eeeeee;
 `
-export const AddresContainer = styled.div`
-display: flex;
-flex-direction: column;
-position:relative;
-width: 100%;
-height: 76px;
-padding: 5px;
-background-color: #a9a9a9;
-
+export const H4 = styled.h4`
+  text-align: center;
 `
-export const AddresTitle = styled.div`
-margin: 8px 0px 0px 16px;
-color: #646464;
-font-family: "Roboto";
-
+export const RenderContainer = styled.div`
+  box-sizing: border-box;
+  margin-bottom: ${props=> props.bottom};
+  padding: 0px;
+  width: 100vw;
+  min-height: 60vh;
+  overflow:scroll;
 `
-export const OrderBar = styled.div`
-width: 360px;
-height: 20px;
-margin-top: 0 0 10px;
-margin:auto;
-margin-bottom:8px;
-text-align: start;
-padding-bottom: 5px; 
-border-bottom: 5px solid #d3d3d3;
-
+export const Title = styled.h1`
+  font-family: Roboto;
+  font-size: 16px;
+  font-weight: bolder;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: -0.39px;
+  text-align: center;
+  color: #000000;
 `
+export const LogoutContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0px 10px;
+`
+
--- src/routes/Router.js
+++ src/routes/Router.js
@@ -1,19 +1,13 @@
 import React from "react";
 import { Switch, Route, BrowserRouter } from "react-router-dom";
-
 import CartPage from "../pages/CartPage/CartPage";
-
 import CreateLoginPage from "../pages/CreateLoginPage/CreateLoginPage"
 import HomePage from "../pages/HomePage/HomePage"
 import HomeScreenPage from "../pages/HomeScreenPage/HomeScreenPage"
 import LoginPage from "../pages/LoginPage/LoginPage"
-
 import ProfilePage from "../pages/ProfilePage/ProfilePage"
- import ProfilePage from "../pages/ProfilePage/ProfilePage"
-
 import RestaurantDetailsPage from "../pages/RestaurantDetailsPage/RestaurantDetailsPage"
 import AddAddressPage from "../pages/AddAddressPage/AddAddressPage";
-import CartPage from "../pages/CartPage/CartPage";
 
 const Router = () => {
     return(
@@ -45,7 +39,7 @@ const Router = () => {
                     <RestaurantDetailsPage/>
                 </Route>
 
-                <Route exact path="/perfil">
+                <Route exact path="/profile">
                     <ProfilePage/>
                 </Route>
                 
